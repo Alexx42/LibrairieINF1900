a449b8d3cab83402747064e0146c0a679f891925
Alexx42 <alexandre.le-goff@polymtl.ca>
2021-03-16 09:58:40 -0400

    boutton finis

--- tp7/librairie/boutonPoussoir.cpp
+++ tp7/librairie/boutonPoussoir.cpp
@@ -1,43 +1,21 @@
 #include "boutonPoussoir.h"
 
-Boutton::Boutton() : estInteruption_(false), port_('d'), broche_(0x02) {
+Boutton::Boutton() : estInteruption_(false) {
 }
 
-Boutton::Boutton(bool a, char p, uint8_t b) : estInteruption_(a), port_(p), broche_(b){
-
+Boutton::Boutton(bool a) : estInteruption_(a) {
+	if (estInteruption_) {
+		init::initINT0();
+	}
 }
 
 Boutton::~Boutton() {
-
 }
 
 bool estAppuyé(){
-    if(port_ == 'A' || port_ == 'a') {
-        if(PINA & (1<<broche_)){
-            _delay_ms(4);
-            return PINA & (1<<broche_);
-        }   
-        return false;
+	if(PIND & PD2) {
+		_delay_ms(4);
+		return PIND & (1 << PD2);
 	}
-	if(port_ == 'B' || port_ == 'b') {
-        if(PINB & (1<<broche_)){
-            _delay_ms(4);
-            return PINB & (1<<broche_);
-        }   
-        return false;
-	}
-	if(port_ == 'C' || port_  == 'c') {
-		if(PINC & (1<<broche_)){
-            _delay_ms(4);
-            return PINC & (1<<broche_);
-        }   
-        return false;
-	}
-	if(port_ == 'D' || port_ == 'd') {
-		if(PIND & (1<<broche_)){
-            _delay_ms(4);
-            return PIND & (1<<broche_);
-        }   
-        return false;
-	}
+	return false;
 }
